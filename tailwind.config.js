173aebe77a070632583f92cde7b6bb5252f013de
Rinoh-Coder <9965rinoh@gmail.com>
2026-02-18 10:48:58 +0300

    config de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
